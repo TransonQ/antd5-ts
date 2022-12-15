8092966acb019f1fae07b15758f0d1504ea91722
quanscheng <quanscheng@163.com>
2022-12-16 00:26:59 +0800

    feat: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter(routes)
 // 三套配色算法: 暗色 darkAlgorithm, 亮色 defaultAlgorithm, 紧凑 compactAlgorithm
 function App() {
   return (
-    <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
+    <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
       <RouterProvider router={router} />
     </ConfigProvider>
   )
