c72d6d40772c73415a86e72c1df8bc2f6145fd7b
quanscheng <quanscheng@163.com>
2022-12-15 16:43:27 +0800

    feat: config 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Button } from 'antd'
+import { Button, ConfigProvider, theme } from 'antd'
 import './App.css'
 
+// 三套配色算法: 暗色 darkAlgorithm, 亮色 defaultAlgorithm, 紧凑 compactAlgorithm
 function App() {
   return (
-    <div className='App'>
-      <Button type='primary'>Button</Button>
-    </div>
+    <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
+      <Button type="primary">Button</Button>
+    </ConfigProvider>
   )
 }
 
